370a5ae8a1b95beeaeaf3f8631bc17f127af2bf5
kangkoilxu <742611817@qq.com>
2019-01-09 17:07:50 +0800

    upate scripts and add samples

--- Table.py
+++ Table.py
@@ -1,6 +1,9 @@
 
 #print Table
 #kangx 01-08-2019
+
+#add table from text
+
 #aling = center
 #align = left
 # +------------+----------+-------+--------------+------------+
@@ -26,8 +29,14 @@ class Table:
         self.align = "center"
         self.hlist = []
         self.lenlist= []
-        self.bcolor = ""
-        self.fcolor = ""
+        self.bcolor = "\033[92m{0}\033[0m"
+        self.fcolor = "\033[93m{0}\033[0m"
+
+
+    def p_list(self):
+        print self.hlist
+        print self.list
+        print self.lenlist
 
     def set_bordercolor(self,ft):
         self.bcolor = "\033[" + str(ft) + "m{0}\033[0m"
@@ -40,6 +49,37 @@ class Table:
             print "\033[91mError: invalid alignment input\033[0m"
             sys.exit()
         self.align = al
+
+    def remove_whitespace_sccharacter(self,slist): # remove whitespace and string control characters
+        templist = []
+        for item in slist:
+            if item not in [ " ",""] :
+                templist.append(item.strip().strip("\n"))
+        return templist
+
+    def add_fromtext(self,txt,spstr=" "):
+        linecnt = 0
+        notitle = False
+        with open(txt,"r") as f:
+            # linescnt = sum(1 for _ in f)
+            # f.seek(0,0)
+            for line in f:
+                if line.strip() == "":
+                    continue
+                if "#no_title" in line:
+                    notitle = True
+                    continue
+                items = line.split(spstr)
+                aitems = self.remove_whitespace_sccharacter(items)
+                # print len(aitems),aitems
+                if not notitle:
+                    if not linecnt: #if it is first line in the file, It should be the heaser
+                        self.addheader(aitems)
+                    else:
+                        self.addrow(aitems)
+                else:
+                    self.addrow(aitems)
+                linecnt += 1
 
     def addheader(self,hlist):
         if type(hlist) != list:
@@ -66,34 +106,50 @@ class Table:
     #list 0 1
 
     def handlelist(self,inlist,is_header = False):
-        if len(inlist):
-            for tlist in inlist:
-                self.body += self.bcolor.format("\n|")
-                for index,item in enumerate(tlist):
-                    if self.align == "center": # space + (half leng space ) + item + (halflen space)
-                        odd = (self.lenlist[index] - len(item) ) % 2
-                        halflen =  (self.lenlist[index] - len(item))/2 + 2#
-                        self.body += self.fcolor.format( " "* halflen +  str(item) + " "*(halflen+odd) )+ self.bcolor.format("|") #if the length of item is odd, add one space to right of item to balance len
-                    elif self.align == "left": #two spaces around the item
-                        self.body += self.fcolor.format( " " + str(item) + " "*(self.lenlist[index] - len(item)) ) + self.bcolor.format(" |")
+        if not len(inlist):
+            return
+        for tlist in inlist:
+            self.body += self.bcolor.format("\n|")
+            for index,item in enumerate(tlist):
+                if self.align == "center": # space + (half leng space ) + item + (halflen space)
+                    odd = (self.lenlist[index] - len(item) ) % 2
+                    halflen =  (self.lenlist[index] - len(item))/2 + 2#
+                    self.body += self.fcolor.format( " "* halflen +  str(item) + " "*(halflen+odd) )+ self.bcolor.format("|") #if the length of item is odd, add one space to right of item to balance len
+                elif self.align == "left": #two spaces around the item
+                    self.body += self.fcolor.format( " " + str(item) + " "*(self.lenlist[index] - len(item)) ) + self.bcolor.format(" |")
 
-                if is_header:
-                    self.body += "\n"+self.bcolor.format(self.header)
+            if is_header:
+                self.body += "\n"+self.bcolor.format(self.header)
 
-    def printlist(self,border=0):
+    def printtable(self,border=0):
         self.lenlist = [] #var to store the max length of item
+
+        if  len(self.hlist) :
+            templist = self.hlist
+        elif  len(self.list) :
+            templist = self.list
+        else :
+            return
         try:
-            for x in range(len(self.list[0])): #Find max length of item
+            for x in range(len(templist[0])): #Find max length of item
                 itemlenMax = 0
-                for tlist in self.list:
-                    if itemlenMax < len(tlist[x]):
-                        itemlenMax = len(tlist[x])
-                for item in self.hlist:
-                    if itemlenMax < len(item[x]):
-                        itemlenMax = len(item[x])
+                try:
+                    if len(self.list) :
+                        for tlist in self.list:
+                            if itemlenMax < len(tlist[x]):
+                                itemlenMax = len(tlist[x])
+                except Exception as e:
+                    print "\033[91mError : invalid input\033[0m",e
+                try:
+                    if len(self.hlist) :
+                        for item in self.hlist:
+                            if itemlenMax < len(item[x]):
+                                itemlenMax = len(item[x])
+                except Exception as e:
+                    print "\033[91mError : invalid input\033[0m",e
                 self.lenlist.append( itemlenMax ) #add to list
         except Exception as e:
-            print "\033[91mError : invalid input\033[0m"
+            print "\033[91mError : invalid input\033[0m",e
             sys.exit()
         #prepare header and tail string
         self.header += "+"
@@ -105,18 +161,10 @@ class Table:
         self.tail = "\n" + self.header
 
         #handle header
-        self.handlelist(self.hlist,is_header = True)
-        self.handlelist(self.list)
+        if len(self.hlist):
+            self.handlelist(self.hlist,is_header = True)
+        if len(self.list):
+            self.handlelist(self.list)
         print self.bcolor.format(self.header) \
         + self.fcolor.format( self.body ) \
         + self.bcolor.format( self.tail)
-
-#test case
-if __name__ == "__main__":
-
-    mt = Table()
-    mt.set_align("center")
-    mt.addheader( ["name","gender","age","birthday","nation"] )
-    mt.addrow(  ["opmdmlwk","male","32","1090-23-90","dhand"] )
-    mt.addrow ( ["oueiwjg","female","83","3o83-20-81","11222222"] )
-    mt.printlist()
--- TestTable.py
+++ TestTable.py
@@ -1,6 +1,7 @@
 import Table
 
 if __name__ == "__main__":
+
     mt = Table.Table()
     mt.set_fontcolor(97)
     mt.set_bordercolor(92)
@@ -9,14 +10,26 @@ if __name__ == "__main__":
     mt.addrow(  ["opmdmlwk","male","32","1090-23-90","dhand"] )
     mt.addrow ( ["oueiwjg","female","83","3o83-20-81","11222222"] )
     mt.addrow ( ["oueiwjg","female","83","3o83-20-81",""] )
-    mt.printlist()
+    mt.printtable()
 
-
+    #
     mt2 = Table.Table()
     mt2.set_fontcolor(36)
     mt2.set_bordercolor(31)
     mt2.set_align("left")
-    mt2.addheader( ["name","gender","age","birthday","nation"] )
-    mt2.addrow(  ["opmdmlddddddd","male","32","1090-23-90","dhand"] )
-    mt2.addrow ( ["oueiwjg","female","83","3o83-20-81","11222222"] )
-    mt2.printlist()
+    mt2.addrow( ["This ","a","test","table","which","doesn't","has","a","header"] )
+    mt2.printtable()
+
+    #read from text
+    mt3 = Table.Table()
+    mt3.set_align("left")
+    mt3.add_fromtext("textsample.txt",spstr = " ")
+    mt3.printtable()
+
+
+    #use custom split characters
+    mt4 = Table.Table()
+    mt4.set_align("left")
+    mt4.add_fromtext("sampletexttable2.txt",spstr = ":")
+    # mt4.p_list()
+    mt4.printtable()
